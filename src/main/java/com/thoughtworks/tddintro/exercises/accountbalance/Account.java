9f4f4a3e17b43f47f2450919a6e38f8a3abeff27
Jackie Robinson <jackierobinsonm@gmail.com>
2017-12-17 01:14:53 -0600

    shouldIncreaseMyBalanceWhenIDepositMoney Test Passed

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,17 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private int balance;
+
+    public Account(int initialBalance) {
+        balance = initialBalance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public Integer getBalance() {
+        return balance;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,21 +3,25 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+        Account account = new Account(100);
+        account.deposit(50);
+        assertThat(account.getBalance(), is(150));
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
+    @Ignore // Remove each @Ignore and implement test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
 
     }
 
     @Test
-    @Ignore  // Remove each @Ignore and implement test
+    @Ignore // Remove each @Ignore and implement test
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
 
     }
